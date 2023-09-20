37df39c71e292bcfd5618f54480920ca38a30fe7
Danijel-James Wynyard <danijel@repasscloud.com>
2023-09-21 00:17:47 +1000

    fix using

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.CookiePolicy;
 
 namespace BlazorOkta
 {
